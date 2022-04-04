03d542d512dab6b9ad83ed5a6d38472565cfc564
nekowasabi <nolifekng00@gmail.com>
2022-04-04 11:32:59 +0900

    Remove unused code.

--- denops/count-word/main.ts
+++ denops/count-word/main.ts
@@ -8,69 +8,11 @@ import {
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
-    async auth(): Promise<unknown> {
-      const { apiKey, apiSecretKey, tokenPath } = await Auth.getSettings(
-        denops
-      );
-
-      const token: string = await Auth.generateToken(
-        apiKey,
-        apiSecretKey,
-        tokenPath,
-        denops
-      );
-
-      try {
-        Auth.saveTokenFromFile(tokenPath, token);
-      } catch (e) {
-        console.error(e);
-      }
-
-      return await Promise.resolve("Authorized complete.");
-    },
-
-    async addTask(task: unknown): Promise<unknown> {
-      ensureString(task);
-      await Auth.addTask(denops, task);
-
-      await denops.cmd(`redraw`);
-      return await Promise.resolve(" add task complete.");
-    },
-
-    async addSelectedTask(
-      start: unknown,
-      end: unknown,
-      _args: unknown
-    ): Promise<void> {
-      const words = await denops.call("getline", start, end);
-      ensureArray(words);
-
-      for (const word of words) {
-        ensureString(word);
-        if (word !== "") {
-          await Auth.addTask(denops, word);
-          await denops.cmd(`redraw`);
-        }
-      }
+    async count(): Promise<unknown> {
     },
 
     async debug(): Promise<void> {},
   };
-
-  await execute(
-    denops,
-    `command! -nargs=0 RtmAuth echomsg denops#request('${denops.name}', 'auth', [])`
-  );
-
-  await execute(
-    denops,
-    `command! -nargs=0 RtmAddTask echomsg denops#request('${denops.name}', 'addTask', [""])`
-  );
-
-  await execute(
-    denops,
-    `command! -nargs=* -range RtmAddSelectedTask echomsg denops#request('${denops.name}', 'addSelectedTask', [<line1>, <line2>, <f-args>])`
-  );
 
   await execute(
     denops,
